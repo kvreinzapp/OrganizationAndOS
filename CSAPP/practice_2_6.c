b6c56f946a3927093c377c6bc6a07af067490db9
klaus <18848113328@163.com>
2022-07-29 21:35:36 +0800

    new book

--- CSAPP/practice_2_6.c
+++ CSAPP/practice_2_6.c
@@ -1,6 +1,36 @@
+// there are 3way to show float in hex
+// 1st function
+// 2ed a satement(seems undefined)
+// 3rd from stackover flow, use union
 #include <stdio.h>
+// 3rd way need include
+#include <stdint.h>
+#include <inttypes.h>
+
+// 1st
+#include "show.h"
+
 int main(void)
 {
     int x = 2607352;
-    printf("0x%.4x", x);
+    printf("0x%08X\n", x);
+
+    float y = 3510593.0;
+    // 2ed
+    printf("0x%08X\n", *(unsigned int *)&y);
+    // show_float(y);
 }
+
+// // 3rd
+// int main(void)
+// {
+//     float val = 3510593.0;
+//     union
+//     {
+//         float f;
+//         uint32_t u;
+//     } f2u = {.f = val};
+
+//     printf("your float : %f\n   : 0x%" PRIx32 "\n", val, f2u.u);
+// }
+// // https://stackoverflow.com/questions/45228925/how-to-print-float-as-hex-bytes-format-in-c
